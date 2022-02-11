3cae600520a4dfb1c1ec5d32eaaa23358b2a4c2e
nwelter1 <nwelter1@gmail.com>
2022-02-11 12:25:34 -0600

    final

--- reportCards.py
+++ reportCards.py
@@ -6,14 +6,14 @@ import json
 pp = pprint.PrettyPrinter(indent=4)
 class GenerateReport:
     def __init__(self, courses, marks, students, tests):
-        self.courses = self.generate_ids(courses)
-        self.marks = self.generate_marks(marks)
-        self.students = self.generate_ids(students)
-        self.tests = self.generate_ids(tests)
+        self.courses = self.process_ids(courses)
+        self.marks = self.process_marks(marks)
+        self.students = self.process_ids(students)
+        self.tests = self.process_ids(tests)
         self.test_course = self.testCourseMerge()
     
     # generating python dicts for all csv records containing an 'id' field
-    def generate_ids(self, file):
+    def process_ids(self, file):
         with open(file) as f:
             csv_reader = csv.reader(f, delimiter=',')
             line_count = 0
@@ -34,7 +34,7 @@ class GenerateReport:
                 d[clean_list[i]['id']] = clean_list[i]
         return d
     # generating python dicts for all csv records containing a 'test_id' field
-    def generate_marks(self, file):
+    def process_marks(self, file):
         with open(file) as f:
             csv_reader = csv.reader(f, delimiter=',')
             line_count = 0
@@ -58,6 +58,7 @@ class GenerateReport:
                     d[clean_list[i]['student_id']] = [clean_list[i]]
             # pp.pprint(d)
             return d
+    # Joining tests and 
     def testCourseMerge(self):
         test_2_course = {}
         for test_id in self.tests:
@@ -68,12 +69,6 @@ class GenerateReport:
             test_2_course[test_id] = [course_id, name, teacher, weight]
         return test_2_course
     
-    def getCourse(self,test_id):
-        courses = self.test2Course()
-        return courses[test_id]
-    
-            
-
     def checkTestWeights(self):
         weight_totals = {}
         weight_check = []
@@ -84,13 +79,11 @@ class GenerateReport:
                 weight_totals[test['course_id']] = int(test['weight'])
         for key in weight_totals:
             if weight_totals[key] != 100:
-                print(self.courses[key])
-                # weight_check.add(self.courses[key])
+                weight_check.add(self.courses[key])
                 print(f'{self.courses[key]["teacher"]}\'s tests have a weight of {weight_totals[key]}. Please adjust to 100 before moving on.')
         if not len(weight_check):
-            return
-        else:
-            print('All courses are weighted properly!')
+            return True
+        return False 
 
     def reportCards(self):
         output = {'students':[]}
@@ -100,16 +93,30 @@ class GenerateReport:
             s_dict = {}
             s_dict['id'] = id
             s_dict['name'] = name
-            s_dict['courses'] = self.calculateAverageClass(id)
+            s_dict['courses'] = self.generateCourseReport(id)
+            print(f'courses = {s_dict["courses"]}')
             s_dict['totalAverage'] = self.calculateTotalAverage(s_dict['courses'])
+            print(s_dict['courses'])
             output['students'].append(s_dict)
         return output
-
-    def calculateTotalAverage(self, grades):
+    def generateCourseReport(self, id):
+        student_courses = []
+        final_grades = self.calculateAverageClass(id)
+        for course_id in final_grades:
+            class_dict = {}
+            class_dict['id'] = course_id
+            class_dict['name'] = self.courses[course_id]['name']
+            class_dict['teacher'] = self.courses[course_id]['teacher']
+            class_dict['courseAverage'] = final_grades[course_id]
+            student_courses.append(class_dict)
+        return student_courses
+    def calculateTotalAverage(self, courses):
+        total = 0
         count = 0
-        for grade in grades.values():
-            count += grade
-        return count/len(grades.values())
+        for grade in courses:
+            total += grade['courseAverage']
+            count += 1
+        return total/count
 
     def calculateAverageClass(self,student_id):
         results = {}
@@ -137,11 +144,23 @@ class GenerateReport:
 
 
 
-report = GenerateReport('courses.csv', 'marks.csv','students.csv','tests.csv')
-# pp.pprint(report.students)
-pp.pprint(report.marks)
-# report.checkTestWeights()
-print(report.testCourseMerge())
-report.calculateAverageClass('2')
-with open('test.json', 'w') as f:
-    json.dump(report.reportCards(), f)
+# report = GenerateReport('courses.csv', 'marks.csv','students.csv','tests.csv')
+# # # pp.pprint(report.students)
+# # pp.pprint(report.marks)
+# # # report.checkTestWeights()
+# # print(report.testCourseMerge())
+# # report.calculateAverageClass('2')
+# with open('test.json', 'w') as f:
+#     json.dump(report.reportCards(), f)
+# print(report.generateCourseReport('1'))
+
+def run(courses, marks, students, tests):
+    report = GenerateReport(courses, marks, students, tests)
+    if not report.checkTestWeights():
+        return 3
+    with open('test.json', 'w') as f:
+        json.dump(report.reportCards(), f)
+    print('Successfully wrote report cards to JSON!')
+
+run('courses.csv', 'marks.csv','students.csv','tests.csv')
+
